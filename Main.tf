50668e0a48eb9112b8302348d5ddb8587367e335
Ivan Karpenko <ubersholder@gmail.com>
2021-04-02 11:34:18 +0300

    working backend now

--- Main.tf
+++ Main.tf
@@ -1,6 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-by-uber"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt         = true
+  }
+}
+
 resource "aws_kms_key" "s3key" {
-description = "Key to encrypt bucket obj"
-deletion_window_in_days = 10
+  description             = "Key to encrypt bucket obj"
+  deletion_window_in_days = 10
 }
 
 resource "aws_s3_bucket" "terraform_state_files" {
@@ -10,15 +20,15 @@ resource "aws_s3_bucket" "terraform_state_files" {
   }
   versioning {
     enabled = true
-    }
-      server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = aws_kms_key.s3key.id
-          sse_algorithm = "aws:kms"
-        }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.s3key.id
+        sse_algorithm     = "aws:kms"
       }
     }
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
