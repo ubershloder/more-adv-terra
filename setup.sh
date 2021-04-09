ebb0b987d5bd998135606be9080400dce63a72bf
Ivan Karpenko <ubersholder@gmail.com>
2021-04-09 15:01:09 +0300

    now only docker

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo mkdir -p /efs 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_mount_target.mount.ip_address}:/  /efs 
 
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
+sudo yum install -y docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+docker run -p 8080:80 nginx -d
